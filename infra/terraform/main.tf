8f531d575834ccbec97d7bc8e0f1ce91ff55fbfa
tontojil <tontojil@gmail.com>
2026-06-28 11:01:09 -0400

    simplify: quitar SQL, usar App Service F1 gratis

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,6 +1,5 @@
 terraform {
   required_version = ">= 1.6"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -20,63 +19,12 @@ resource "azurerm_resource_group" "main" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "main" {
-  name                = "vnet-perfeck-rankings"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  address_space       = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "app" {
-  name                 = "snet-app"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_subnet" "db" {
-  name                 = "snet-db"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_mssql_server" "main" {
-  name                         = "sql-perfeck-rankings"
-  resource_group_name          = azurerm_resource_group.main.name
-  location                     = azurerm_resource_group.main.location
-  version                      = "12.0"
-  administrator_login          = var.sql_admin_login
-  administrator_login_password = var.sql_admin_password
-}
-
-resource "azurerm_mssql_database" "main" {
-  name      = "perfeckrankings-db"
-  server_id = azurerm_mssql_server.main.id
-  sku_name  = "S0"
-  max_size_gb = 2
-}
-
-resource "azurerm_private_endpoint" "sql" {
-  name                = "pe-sql"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  subnet_id           = azurerm_subnet.db.id
-
-  private_service_connection {
-    name                           = "psc-sql"
-    private_connection_resource_id = azurerm_mssql_server.main.id
-    subresource_names              = ["sqlServer"]
-    is_manual_connection           = false
-  }
-}
-
 resource "azurerm_service_plan" "main" {
   name                = "asp-perfeck-rankings"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "api" {
@@ -94,12 +42,7 @@ resource "azurerm_linux_web_app" "api" {
   }
 
   app_settings = {
-    "ASPNETCORE_ENVIRONMENT"               = "Production"
-    "ConnectionStrings__DefaultConnection" = "Server=tcp:${azurerm_mssql_server.main.fully_qualified_domain_name},1433;Database=${azurerm_mssql_database.main.name};User ID=${var.sql_admin_login};Password=${var.sql_admin_password};Encrypt=true;TrustServerCertificate=false;"
-  }
-
-  identity {
-    type = "SystemAssigned"
+    "ASPNETCORE_ENVIRONMENT" = "Production"
   }
 }
 
@@ -116,35 +59,4 @@ resource "azurerm_linux_web_app" "frontend" {
     }
     always_on = false
   }
-
-  app_settings = {
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-  }
-}
-
-resource "azurerm_network_security_group" "api" {
-  name                = "nsg-perfeck-api"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-
-  security_rule {
-    name                       = "AllowHTTPS"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "443"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_storage_account" "main" {
-  name                     = "stperfeckrankings"
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  min_tls_version          = "TLS1_2"
 }
